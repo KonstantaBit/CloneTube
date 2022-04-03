529ca44d08afd0fab574cedec888e35f0414cd48
KonstantaBit <jaykonstanta@mail.ru>
2022-04-03 23:49:13 +0300

    file system created

---
+++ manage.py
@@ -0,0 +1,12 @@
+import argparse
+from settings import app
+
+# Здесь будут описаны функции для работы с проектом через командную строку
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', type=str)
+    args = parser.parse_args()
+    if args.command == 'runserver':
+        app.run(port=8080, host='127.0.0.1')
+    else:
+        print(f"Unknown command: '{args.command}'\nType 'manage.py -h' for usage.")
