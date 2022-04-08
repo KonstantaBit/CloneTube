da51bc32e94af07bb6c713338d2f859d72f92250
KonstantaBit <jaykonstanta@mail.ru>
2022-04-08 21:55:44 +0300

    manage.py repair

--- manage.py
+++ manage.py
@@ -2,12 +2,11 @@ import argparse
 from settings import *
 
 # Здесь будут описаны функции для работы с проектом через командную строку
-app.run(port=8080, host='127.0.0.1', debug=DEBUG)
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('command', type=str)
-#     args = parser.parse_args()
-#     if args.command == 'runserver':
-#         app.run(port=8080, host='127.0.0.1', debug=DEBUG)
-#     else:
-#         print(f"Unknown command: '{args.command}'\nType 'manage.py -h' for usage.")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', type=str)
+    args = parser.parse_args()
+    if args.command == 'runserver':
+        app.run(port=8080, host='127.0.0.1', debug=DEBUG)
+    else:
+        print(f"Unknown command: '{args.command}'\nType 'manage.py -h' for usage.")
